8ad531c607db4760a6bd9770899747d9200608a8
Ruoran Liu <rrliu@thoughtworks.com>
2015-03-27 16:27:05 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var browserify = require('gulp-browserify');
+var connect = require('gulp-connect');
+var del = require('del');
+var fs = require('fs');
+
+var config = {
+  js: {
+    path: 'js/',
+    extension: '.js'
+  },
+  sass: {
+    path: 'scss/',
+    extension: '.scss'
+  },
+  css: {
+    path: 'css/'
+  },
+  asset: {
+    path: 'asset/'
+  },
+  jade: {
+    path: 'jade/',
+    extension: '.jade'
+  },
+  build: {
+    path: 'build/'
+  },
+  data: {
+    path: './data.json'
+  },
+  port: 8000
+};
+
+gulp.task('clean', function(cb){
+  del([config.build.path], cb);
+});
+
+gulp.task('js', function(){
+  return gulp.src(config.js.path + '*' + config.js.extension)
+             .pipe(browserify())
+             .pipe(gulp.dest(config.build.path + config.js.path));
+});
+
+gulp.task('sass', function(){
+  return gulp.src(config.sass.path + '**/*' + config.sass.extension)
+             .pipe(sass())
+             .pipe(gulp.dest(config.build.path + config.css.path));
+});
+
+gulp.task('asset', function(){
+  return gulp.src(config.asset.path + '**/*')
+             .pipe(gulp.dest(config.build.path + config.asset.path));
+});
+
+gulp.task('jade', function(){
+  return gulp.src([config.jade.path + '/**/*' + config.jade.extension,
+                   '!' + config.jade.path + '**/_*' + config.jade.extension])
+             .pipe(jade())
+             .pipe(gulp.dest(config.build.path));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(config.js.path + '**/*' + config.js.extension, ['js']);
+  gulp.watch(config.sass.path + '**/*' + config.sass.extension, ['sass']);
+  gulp.watch(config.asset.path + '**/*', ['asset']);
+  gulp.watch(config.jade.path + '**/*' + config.jade.extension, ['jade']);
+  gulp.watch(config.data.path, ['jade']);
+});
+
+gulp.task('server', function(){
+  connect.server({
+    root: config.build.path,
+    port: config.port || 8000
+  });
+});
+
+gulp.task('build', ['clean'], function(){
+  gulp.start(['js', 'sass', 'asset', 'jade']);
+});
+
+gulp.task('default', ['watch', 'build', 'server']);
