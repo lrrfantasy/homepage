04d8a635f6e2d7fad8e48b74e73412a931e90d95
Ruoran Liu <rrliu@thoughtworks.com>
2015-06-18 17:10:42 +0800

    first version of travel

--- js/index.js
+++ js/index.js
@@ -1,5 +1,81 @@
-import $ from 'jquery'
+import _ from 'lodash'
+import Trip from './trip'
 
-$(() => {
-  console.log($.fn.jquery)
-})
+let japanMap = new Trip('japan', [140, 39], 1400)
+japanMap.init()
+japanMap.places([
+  {
+    name: 'Tokyo',
+    latitude: 35.6833,
+    longitude: 139.6833
+  },
+  {
+    name: 'Nagoya',
+    latitude: 35.1833,
+    longitude: 136.9
+  },
+  {
+    name: 'Osaka',
+    latitude: 34.6939,
+    longitude: 135.5022
+  }
+])
+japanMap.routes([['Tokyo', 'Nagoya'], ['Nagoya', 'Osaka']])
+
+let australiaMap = new Trip('australia', [135, -30], 600)
+australiaMap.init()
+australiaMap.places([
+  {
+    name: 'Sydney',
+    latitude: -33.865,
+    longitude: 151.2094
+  },
+  {
+    name: 'Melbourne',
+    latitude: -37.8136,
+    longitude: 144.9631
+  },
+  {
+    name: 'Brisbane',
+    latitude: -27.4667,
+    longitude: 153.0333
+  }
+])
+australiaMap.routes([['Sydney', 'Melbourne'], ['Sydney', 'Brisbane']])
+
+let swedenMap = new Trip('sweden', [18, 62], 1000)
+swedenMap.init()
+swedenMap.places([
+  {
+    name: 'Moscow',
+    latitude: 55.75,
+    longitude: 37.6167
+  },
+  {
+    name: 'St Petersburg',
+    latitude: 59.95,
+    longitude: 30.3
+  },
+  {
+    name: 'Helsinki',
+    latitude: 60.1708,
+    longitude: 24.9375
+  },
+  {
+    name: 'Stockholm',
+    latitude: 59.3294,
+    longitude: 18.0686
+  },
+  {
+    name: 'Oslo',
+    latitude: 59.95,
+    longitude: 10.75
+  },
+  {
+    name: 'Copenhagen',
+    latitude: 55.6761,
+    longitude: 12.5683
+  }
+])
+swedenMap.routes([['Moscow', 'St Petersburg'], ['St Petersburg', 'Helsinki'], ['Helsinki', 'Stockholm'], ['Stockholm', 'Oslo'], ['Oslo', 'Copenhagen']])
+
---
+++ js/trip.js
@@ -0,0 +1,87 @@
+import d3 from 'd3'
+import _ from 'lodash'
+import Datamap from './vendor/datamap'
+
+const defaultConfig = {
+  projection: 'mercator',
+  geographyConfig: {
+    borderColor: '#79b6d2',
+    highlightFillColor: 'rgba(0, 0, 0, 0.05)',
+    highlightBorderColor: '#79b6d2'
+  },
+  bubblesConfig: {
+    borderWidth: 0,
+    fillOpacity: 1,
+    highlightFillColor: '#ff8f8f',
+    highlightBorderWidth: 0,
+    highlightFillOpacity: 1
+  },
+  arcConfig: {
+    strokeColor: '#cd5c5c',
+    greatArc: true
+  },
+  fills: {
+    defaultFill: 'rgba(0, 0, 0, 0.2)',
+    city: '#cd5c5c'
+  }
+}
+
+function zoom(center, scale) {
+  return (element) => {
+    let projection = d3.geo.mercator()
+      .center(center)
+      .scale(scale)
+      .translate([element.offsetWidth / 2, element.offsetHeight / 2])
+    let path = d3.geo.path().projection(projection)
+    return {path: path, projection: projection}
+  }
+}
+
+class Trip {
+  constructor(name, zoomCenter, scale) {
+    this.config = _.merge({
+        element: document.getElementById(name),
+        setProjection: zoom(zoomCenter, scale)
+      }, defaultConfig)
+  }
+
+  init() {
+    this.instance = new Datamap(this.config)
+    return this.instance
+  }
+
+  places(places) {
+    this.places = places
+    let bubbles = places.map((place) => {
+      return {
+        name: place.name,
+        radius: 5,
+        fillKey: 'city',
+        latitude: place.latitude,
+        longitude: place.longitude
+      }
+    })
+    this.instance.bubbles(bubbles)
+  }
+
+  routes(routes) {
+    this.routes = routes
+    let arcs = routes.map((route) => {
+      return [this.getPlaceByName(route[0]), this.getPlaceByName(route[1])]
+    }).map((route) => {
+      return {
+        origin: route[0],
+        destination: route[1]
+      }
+    })
+    this.instance.arc(arcs)
+  }
+
+  getPlaceByName(name) {
+    return this.places.filter((place) => {
+      return place.name === name
+    })[0]
+  }
+}
+
+export default Trip
