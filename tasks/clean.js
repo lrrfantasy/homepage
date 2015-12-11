3109cdc1ebc531f5d4efa5ffd3e7e60f6c091f40
Ruoran Liu <rrliu@thoughtworks.com>
2015-12-11 17:51:29 +0800

    ignore .git folder when cleaning

--- tasks/clean.js
+++ tasks/clean.js
@@ -2,5 +2,5 @@ import del from 'del'
 import config from './config'
 
 export default (cb) => {
-  del([config.build.path], cb)
+  del([config.build.path + '**/*', '!' + config.build.path + '.git/'], cb)
 }
