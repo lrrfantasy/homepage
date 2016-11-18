98badaed91d48511e233c74e6dfade7a7d729061
Ruoran Liu <rrliu@thoughtworks.com>
2016-11-18 17:36:31 +0800

    switch douban api to https

--- src/actions/read.js
+++ src/actions/read.js
@@ -5,7 +5,7 @@ import jsonp from 'superagent-jsonp'
 
 export const fetchReadList = createAction('fetch read list', async () => {
   return await new Promise((resolve, reject) => {
-    superagent.get('http://api.douban.com/v2/book/user/lrrfantasy/collections?count=20')
+    superagent.get('https://api.douban.com/v2/book/user/lrrfantasy/collections?count=20')
       .use(jsonp)
       .end((err, res) => {
         resolve(res.body)
